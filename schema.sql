d6eb39d72f70349e9cc16df2bede99c4a89d9cbd
sasw <saswain96@gmail.com>
2022-12-08 15:49:46 -0600

    table seeds populated

--- schema.sql
+++ schema.sql
@@ -28,5 +28,8 @@ CREATE TABLE employee (
     manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
+    PRIMARY KEY (id)
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
     ON DELETE SET NULL
 )
